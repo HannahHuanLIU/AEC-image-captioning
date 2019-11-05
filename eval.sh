ff5a3238e138d7816cf6d696293061089c1a3b31
HannahHuanLIU <43946749+HannahHuanLIU@users.noreply.github.com>
2019-11-06 00:08:56 +0800

    Delete eval.sh

--- eval.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for file in ./models/*.npy
-do
-    filename=$(basename "$file")
-    filename="${filename%.*}"
-    python main.py --phase=eval --model_file="${file}" > "${filename}.txt"
-done
-exit 0
